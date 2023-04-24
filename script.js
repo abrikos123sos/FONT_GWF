083629a99e9b6e4fe96ad704e176a00afdb83231
abrikos123sos <vlad.shepilka@mail.ru>
2023-04-24 16:15:41 +0300

    On branch maketwork  Your branch is up to date with 'origin/maketwork'.  Changes to be committed: 	new file:   images/deniss.png 	modified:   images/photo2.png 	modified:   images/photo3.png 	modified:   maket.html 	new file:   menu.css 	modified:   script.js 	modified:   style.css 	modified:   style.css.map 	modified:   style.scss

--- script.js
+++ script.js
@@ -7,12 +7,10 @@ function slideShow() {
     for (i = 0; i < img.length; i++) {
         img[i].style.display = "none";
     }
-
     changePhoto++;
     if (changePhoto > img.length) {
         changePhoto = 1;
     }
     img[changePhoto - 1].style.display = "block";
 }
-
 slideShow();
