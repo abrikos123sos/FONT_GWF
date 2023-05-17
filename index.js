b68e32cf42cea4defc4cc0ebbfc8da0e4c162fa1
abrikos123sos <vlad.shepilka@mail.ru>
2023-05-17 18:15:49 +0300

    On branch homeworkjs  Your branch is up to date with 'origin/homeworkjs'.  Changes to be committed: 	modified:   index.js

--- index.js
+++ index.js
@@ -2,3 +2,19 @@ function getValueSum() {
     let sumSplit = document.getElementById("numbers").value.split(" ").map(x => parseInt(x)).reduce((x, y) => x + y);
     document.getElementById('res').innerHTML = 'Сумма: ' + sumSplit;
 }
+
+
+function string(str) {
+    let start = 0;
+    let end = str.length;
+    if (str.charAt(0) == 'm' && str.charAt(str.length - 1) == 'J') {
+        start = 1;
+        end--;
+    } else {
+        return str;
+    }
+
+    return str.substring(start, end);
+}
+console.log(string("JavaScript"));
+console.log(string("JavaScriptJ"));
